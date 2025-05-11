a2e63995ffc300db290f5dd4f0ac56a15443d43f
Ayaz <ixem-88@mail.ru>
2025-05-11 22:48:17 +0400

    Update webpack.config.js

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'production', // Bu sətiri əlavə edin
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
