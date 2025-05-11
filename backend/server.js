badc1f91bac6463932a3db0a02e478430f7b3be0
Ayaz <ixem-88@mail.ru>
2025-05-11 22:14:05 +0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-﻿const express = require('express');
+const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const productsRouter = require('./routes/products');
@@ -7,19 +7,16 @@ require('dotenv').config();
 
 const app = express();
 
-// Təhlükəsizlik üçün middleware-lər
 app.use(helmet());
 app.use(cors({
-  origin: 'http://localhost:3000', // Frontend-in ünvanı
+  origin: 'https://online-store-frontend.onrender.com',
   credentials: true,
 }));
 app.use(express.json());
 
-// Routelar
 app.use('/products', productsRouter);
 app.use('/categories', categoriesRouter);
 
-// Serverin işə salınması
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server http://localhost:${PORT} ünvanında işləyir`);
