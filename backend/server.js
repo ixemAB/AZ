dfeeed24f73f143055f2eaca89f8224d9ed31451
Ayaz <ixem-88@mail.ru>
2025-05-12 20:10:31 +0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,21 +3,30 @@ const cors = require('cors');
 const helmet = require('helmet');
 const productsRouter = require('./routes/products');
 const categoriesRouter = require('./routes/categories');
-require('dotenv').config();
+const dotenv = require('dotenv');
+const fs = require('fs');
+const path = require('path');
 
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(helmet());
+// data qovluğunu yoxla
+const dataDir = path.join(__dirname, 'data');
+if (!fs.existsSync(dataDir)) {
+  fs.mkdirSync(dataDir);
+  console.log('data qovluğu yaradıldı.');
+}
+
+app.use(express.json());
 app.use(cors({
-  origin: 'https://online-store-frontend-skr8.onrender.com',
+  origin: 'https://online-store-frontend.onrender.com',
   credentials: true,
 }));
-app.use(express.json());
-
+app.use(helmet());
 app.use('/products', productsRouter);
 app.use('/categories', categoriesRouter);
 
-const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server http://localhost:${PORT} ünvanında işləyir`);
+  console.log(`Server ${PORT}-ci portda işləyir`);
 });
