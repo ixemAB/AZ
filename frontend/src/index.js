5ca5a8d3e3b49d3bdabaaa4644cff3e8c17cd621
Ayaz <ixem-88@mail.ru>
2025-05-12 20:06:35 +0400

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,10 @@
-﻿import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
