5f3fcb3b9760adf7b1746568fea3029f945b93c8
Ayaz <ixem-88@mail.ru>
2025-05-12 20:09:27 +0400

    Update products.js

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,4 +1,5 @@
-﻿const express = require('express');
+// products.js
+const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const path = require('path');
@@ -6,10 +7,12 @@ const path = require('path');
 const productsFile = path.join(__dirname, '../data/products.json');
 
 router.get('/', (req, res) => {
-  fs.readFile(productsFile, 'utf8', (err, data) => {
-    if (err) return res.status(500).json({ error: 'Məlumat oxuna bilmədi' });
+  try {
+    const data = fs.readFileSync(productsFile, 'utf8');
     res.json(JSON.parse(data));
-  });
+  } catch (err) {
+    res.status(500).json({ error: 'Məlumat oxuna bilmədi: ' + err.message });
+  }
 });
 
 module.exports = router;
